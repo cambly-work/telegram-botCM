39f25f4ad5e81076240ab3db269d538fa4e1d13e
Tim Blau <81749507+cambly-work@users.noreply.github.com>
2025-10-02 02:36:14 -0300

    Support whitespace-delimited admin IDs

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -21,3 +21,21 @@ def test_admin_ids_skips_invalid_tokens():
         else:
             os.environ["ADMIN_IDS"] = original_admin_ids
         importlib.reload(settings_module)
+
+
+def test_admin_ids_supports_whitespace_delimiters():
+    """Whitespace-delimited IDs should also be parsed correctly."""
+    import settings as settings_module
+
+    original_admin_ids = os.environ.get("ADMIN_IDS")
+    os.environ["ADMIN_IDS"] = "101  202\n303"
+
+    importlib.reload(settings_module)
+    try:
+        assert settings_module.ADMIN_IDS == {101, 202, 303}
+    finally:
+        if original_admin_ids is None:
+            os.environ.pop("ADMIN_IDS", None)
+        else:
+            os.environ["ADMIN_IDS"] = original_admin_ids
+        importlib.reload(settings_module)
